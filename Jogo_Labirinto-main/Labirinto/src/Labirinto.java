3a1441cec12d0e10d40d27bdee8e1de0da43c345
felipecal <fcalldas@gmail.com>
2021-09-17 22:01:45 -0300

    Commmit

--- Jogo_Labirinto-main/Labirinto/src/Labirinto.java
+++ Jogo_Labirinto-main/Labirinto/src/Labirinto.java
@@ -53,7 +53,7 @@ public class Labirinto {
             System.out.println();
         }
         try {
-            Thread.sleep(350);
+            Thread.sleep(400);
         }
         catch (InterruptedException e) {
             e.printStackTrace();
